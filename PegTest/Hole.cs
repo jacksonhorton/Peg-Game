154b4ee6344d5d7f34a272b0fc5d033a2d306991
Jackson Horton <jacrhort@ut.utm.edu>
2023-04-10 09:48:41 -0500

    Documenting Hole and Peg class

--- PegTest/Hole.cs
+++ PegTest/Hole.cs
@@ -2,10 +2,9 @@
  * @file:
  * @authors: William Hayes & Jackson Horton
  * @date:4/6/2023
- * @brief:
- * 
- * 
- * 
+ * @brief: Holes represent places where pegs can be. There is always
+ * a constant number of holes for a given board. Position may or may
+ * not contain a peg.
  */
 using System;
 using System.Collections.Generic;
@@ -18,18 +17,31 @@ namespace PegTest
 {
     public class Hole
     {
-        protected Peg peg;
+        // Holes contain pegs
+        private Peg peg;
 
+        /**
+         * Constructor
+         * @param   position    index position of the hole on the board
+         * @param   filled      whether or not the hole has a peg in it
+         */
         public Hole(int position, bool filled)
         {
             peg = new Peg(position, filled);
         }
         
+        /*
+         * Getter for the isFilled attribute of the Peg object
+         * @return  bool    Whether or not the contained Peg is filled
+         */
         public bool isFilled()
         {
             return peg.isFilled();
         }
-
+        /*
+         * Setter for the isFilled attribute of the Peg object
+         * @param   filled  Update to if the contained peg is filled
+         */
         public void setFilled(bool filled)
         {
             peg.setFilled(filled);
@@ -38,18 +50,6 @@ namespace PegTest
         public int GetPosition()
         {
             return peg.getPosition();
-        }
-
-        public Peg GetPeg()
-        {
-            return peg;
-        }
-
-        //public void RemovePeg()
-
-        public void SetFilled(bool filled)
-        {
-            peg.setFilled(filled);
         }
     }
 }
--- PegTest/Peg.cs
+++ PegTest/Peg.cs
@@ -1,11 +1,8 @@
 ﻿/*
- * @file:
  * @authors: William Hayes & Jackson Horton
  * @date:4/6/2023
- * @brief:
- * 
- * 
- * 
+ * @brief: Peg is used to represent each peg on the board. A peg goes
+ * with a hole on the board. 
  */
 using System;
 using System.Collections.Generic;
@@ -21,20 +18,37 @@ namespace PegTest
         private int position;
         private bool filled;
 
+        /**
+         * Constructor
+         * @param   position    The index of the peg, correlated to the index of a hole on the board
+         * @param   isFilled    Is the peg filled or not/still a valid peg to be moved
+         */
         public Peg(int position, bool isFilled)
         {
             this.position = position;
             this.filled = isFilled;
         }
 
+        /**
+         * Getter for isFilled
+         * @return  bool    isFilled instance variable
+         */
         public bool isFilled()
         {
             return filled;
         }
+        /**
+         * Setter for isFilled
+         * @param   filled    is the peg now filled or not
+         */
         public void setFilled(bool filled)
         {
             this.filled = filled;
         }
+        /**
+         * Getter for position
+         * @return  int the index of this object
+         */
         public int getPosition()
         {
             return position;
